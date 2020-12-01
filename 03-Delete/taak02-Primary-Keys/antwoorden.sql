87fa4c2d8cc8e0d427c26826a95f2a00a80d332d
Joey janmaat <Joey.janmaat@student.rocva.nl>
2020-12-01 10:55:50 +0100

    Antwoord php

--- 03-Delete/taak02-Primary-Keys/antwoorden.sql
+++ 03-Delete/taak02-Primary-Keys/antwoorden.sql
@@ -1,5 +1,8 @@
 -- Opdracht 3
-
+SELECT * FROM `players` WHERE id = 198329
 -- Opdracht 4
-
+SELECT * FROM `players` WHERE id = 199304
 -- Opdracht 5
+SELECT * FROM `players` WHERE id = 207494
+-- Opdracht 6
+SELECT * FROM `players` WHERE id = 226753
